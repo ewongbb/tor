bf5f124e6afc2aefc3d9e43b15bdde999452382d
Edmund Wong <ewongbb@pw-wspx.org>
2017-10-04 00:46:54 +0800

    Fixed src/common/crypto_ed25519.h

--- src/common/crypto_ed25519.h
+++ src/common/crypto_ed25519.h
@@ -51,15 +51,15 @@ int ed25519_keypair_generate(ed25519_keypair_t *keypair_out, int extra_strong);
 int ed25519_sign(ed25519_signature_t *signature_out,
                  const uint8_t *msg, size_t len,
                  const ed25519_keypair_t *key);
-MOCK_DECL(int,ed25519_checksig,(const ed25519_signature_t *signature,
-                                const uint8_t *msg, size_t len,
-                                const ed25519_public_key_t *pubkey));
+MOCK_DECL(int, ed25519_checksig, (const ed25519_signature_t *signature,
+                                  const uint8_t *msg, size_t len,
+                                  const ed25519_public_key_t *pubkey));
 
 MOCK_DECL(int,
-ed25519_sign_prefixed,(ed25519_signature_t *signature_out,
-                       const uint8_t *msg, size_t len,
-                       const char *prefix_str,
-                       const ed25519_keypair_t *keypair));
+ed25519_sign_prefixed, (ed25519_signature_t *signature_out,
+                        const uint8_t *msg, size_t len,
+                        const char *prefix_str,
+                        const ed25519_keypair_t *keypair));
 
 int
 ed25519_checksig_prefixed(const ed25519_signature_t *signature,
@@ -84,9 +84,9 @@ typedef struct {
   size_t len;
 } ed25519_checkable_t;
 
-MOCK_DECL(int, ed25519_checksig_batch,(int *okay_out,
-                                       const ed25519_checkable_t *checkable,
-                                       int n_checkable));
+MOCK_DECL(int, ed25519_checksig_batch, (int *okay_out,
+                                        const ed25519_checkable_t *checkable,
+                                        int n_checkable));
 
 int ed25519_keypair_from_curve25519_keypair(ed25519_keypair_t *out,
                                             int *signbit_out,
